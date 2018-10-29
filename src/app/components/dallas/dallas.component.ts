f8c9bd435e7cfb988c08e784b9d3f53a28cdfe77
Kevin Stradtman <stradtkt@gmail.com>
2018-10-29 13:56:31 -0400

    Added the bid cities component files

--- src/app/components/dallas/dallas.component.ts
+++ src/app/components/dallas/dallas.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { WeatherService } from '../../services/weather.service';
 @Component({
   selector: 'app-dallas',
   templateUrl: './dallas.component.html',
   styleUrls: ['./dallas.component.css']
 })
 export class DallasComponent implements OnInit {
-
-  constructor() { }
+  constructor(private weatherService: WeatherService) { }
+  humidity: number;
+  avgTemp: number;
+  highTemp: number;
+  lowTemp: number;
+  status: string;
 
   ngOnInit() {
+    this.getDallasWeather();
   }
-
+  getDallasWeather() {
+    const observable = this.weatherService.getData(4684888);
+    observable.subscribe(data => {
+      this.humidity = data.main.humidity;
+      this.status = data.weather[0].description;
+      this.highTemp = data.main.temp_max;
+      this.lowTemp = data.main.temp_min;
+      this.avgTemp = Math.round((this.highTemp + this.lowTemp) / 2);
+    });
+  }
 }
--- src/app/components/seattle/seattle.component.ts
+++ src/app/components/seattle/seattle.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+
 
 @Component({
   selector: 'app-seattle',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seattle.component.css']
 })
 export class SeattleComponent implements OnInit {
-
-  constructor() { }
+  constructor(private weatherService: WeatherService) { }
+  humidity: number;
+  avgTemp: number;
+  highTemp: number;
+  lowTemp: number;
+  status: string;
 
   ngOnInit() {
+    this.getSeattleWeather();
+  }
+  getSeattleWeather() {
+    const observable = this.weatherService.getData(5809844);
+    observable.subscribe(data => {
+      this.humidity = data.main.humidity;
+      this.status = data.weather[0].description;
+      this.highTemp = data.main.temp_max;
+      this.lowTemp = data.main.temp_min;
+      this.avgTemp = Math.round((this.highTemp + this.lowTemp) / 2);
+    });
   }
 
 }
